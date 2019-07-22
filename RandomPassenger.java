da1f97edb7adbe87f61c10d1f3fe2d9434140dda
frankelms <43014076+frankelms@users.noreply.github.com>
2019-07-22 12:14:07 -0400

    Changed 5 floors to 100

--- RandomPassenger.java
+++ RandomPassenger.java
@@ -10,13 +10,13 @@ public class RandomPassenger
     public Passenger randPass() 
     {
         Random rand = new Random();
-        int s = rand.nextInt(5); 
+        int s = rand.nextInt(100); 
         int s += 1;
-        int d = rand.nextInt(5);
+        int d = rand.nextInt(100);
         int d += 1; 
         while (d == s) 
         {
-            d = rand.nextInt(5);
+            d = rand.nextInt(100);
             d += 1;
         }
         Passenger randPass = new Passenger(s, d);
