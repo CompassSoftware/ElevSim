fb87bd3a3248124c62003d8224246b0e2acf7181
frankelms <43014076+frankelms@users.noreply.github.com>
2019-07-22 11:12:09 -0400

    Created basic createPassenger method

--- RandomPassenger.java
+++ RandomPassenger.java
@@ -5,9 +5,20 @@
 
 import java.util.Random;
 
-public class RandomPassenger {
-  public void setStartFloor() {
-    Random rand = new Random();
-    int f = rand.nextInt();    
-  }
+public class RandomPassenger 
+{
+    public void createPassenger() 
+    {
+        Random rand = new Random();
+        int s = rand.nextInt(5); 
+        int s += 1;
+        int d = rand.nextInt(5);
+        int d += 1; 
+        while (d == s) 
+        {
+            d = rand.nextInt(5);
+            d += 1;
+        }
+    Passenger randPass = new Passenger(s, d);
+    }
 }
